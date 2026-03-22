72d8aa23283c1418cf41500a345dff73fa06c860
Matheus Faustino <matheus.sfp97@gmail.com>
2026-03-22 22:16:12 +0100

    add global styles, styles for header and footer, add SASS, add pages initial files

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,15 @@
 import { NavLink } from 'react-router-dom'
-import logo from '../../assets/logo-header'
-import './Header.css'
+import logo from '../../assets/logo-header.png'
+import '../../main.css'
 
 function Header() {
     return (
         <header>
-            <div className="header-container">
-                <img src={logo} alt="Kasa logo" className="header-logo" />
-                <nav className="nav">
-                    <NavLink to="/" end="" className="nav-link">Accueil</NavLink>
-                    <NavLink to="/about" className="nav-link">À Propos</NavLink>
+            <div className="header__container">
+                <img src={logo} alt="Kasa logo" className="header__logo" />
+                <nav className="header__nav">
+                    <NavLink to="/" end="" className="nav__link">Accueil</NavLink>
+                    <NavLink to="/about" className="nav__link">À Propos</NavLink>
                 </nav>
             </div>
         </header>
