ccc7fd360a36472c4cd4e01cc24a1205b3381fb6
Matheus Faustino <matheus.sfp97@gmail.com>
2026-03-24 00:16:33 +0100

    add homepage

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,13 +1,9 @@
 import '../../main.css'
 
 function Banner() {
-    const sectionClass = document.queryselector('.nav__link.active').className.includes('home') ? 
-        'homepage' : 'about'
-
     return (
-        <section className={`banner ${sectionClass}`}>
-            {document.queryselector('.nav__link.active').className.includes('home') ? 
-            (<h1 className="banner__text">Chez vous, partout et ailleurs</h1>) : ''}
+        <section className="banner homepage">
+            <h1 className="banner__text">Chez vous, partout et ailleurs</h1>
         </section>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,21 @@
 import { accomodationsData } from '../../data/logements'
+import Banner from '../../components/Banner'
+import AccomodationCard from '../../components/Card'
+import '../../main.css'
 
 function Home() {
-    // const recentAccomodations = accomodationsData.slice(0, 6)
+     const recentAccomodations = accomodationsData.slice(0, 6)
     
-    //     return (
-    //         <section className="accomodations">
-    //             {recentAccomodations.map((accomodation) => (
-    //                 <AccomodationCard key={accomodation.id} accomodation={accomodation} />
-    //             ))}
-    //         </section>
-    //     )
+         return (
+            <div className="page">
+                <Banner />
+                <section className="accomodations">
+                    {recentAccomodations.map((accomodation) => (
+                        <AccomodationCard key={accomodation.id} accomodation={accomodation} />
+                    ))}
+                </section>
+            </div>
+         )
 }
 
 export default Home
