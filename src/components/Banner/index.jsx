b0c81aa5c826be1b7523fdf17e988605a103270a
Matheus Faustino <matheus.sfp97@gmail.com>
2026-03-24 21:55:17 +0100

    small corrections

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,7 @@ import '../../main.css'
 
 const Banner = ({ isHome }) => {
   return (
-        <section className={`banner ${isHome && 'homepage'}`}>
+        <section className={`banner ${isHome ? 'homepage' : 'about'}`}>
             {isHome && <h1 className="banner__text">Chez vous, partout et ailleurs</h1>}
         </section>
     )
