0860c6788d34bf810f7baf1e87112ad5dae4ef15
Matheus Faustino <matheus.sfp97@gmail.com>
2026-03-29 22:04:44 +0200

    add error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../main.css";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className="page">
+      <section className="error">
+        <h1 className="error__title">404</h1>
+        <p className="error__message">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/" className="error__link">
+          Retourner sur la page d'accueil
+        </Link>
+      </section>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
