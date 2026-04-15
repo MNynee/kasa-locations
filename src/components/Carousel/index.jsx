96cd34ea2a5bb6daaefea509960f806ec14dc203
Matheus Faustino <matheus.sfp97@gmail.com>
2026-04-15 20:00:49 +0200

    Fix fetching adress on homepage, add counter to carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -31,6 +31,7 @@ const Carousel = ({ accomodation }) => {
           <img key={`${accomodation.id}-${index}`} src={picture} alt={`Picture ${index + 1}`} className="carousel__img" />
         ))}
       </div>
+      <span className="carousel__index">{currentIndex + 1}/{accomodation.pictures.length}</span>
       <img src="../../src/assets/arrow-left.svg" alt="Previous photo" className="carousel__arrow left" onClick={() => handlePrevious()} />
       <img src="../../src/assets/arrow-right.svg" alt="Next photo" className="carousel__arrow right" onClick={() => handleNext()} />
     </section>
