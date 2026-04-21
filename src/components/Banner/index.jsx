46240bdffc836d4f45681b9ee229f8f47b836f2d
Matheus Faustino <matheus.sfp97@gmail.com>
2026-04-21 20:21:13 +0200

    put banner imgs in public directory

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,9 +1,11 @@
 const Banner = ({ isHome }) => {
   return (
-        <section className={`banner ${isHome ? 'homepage' : 'about'}`}>
-            {isHome && <h1 className="banner__text">Chez vous, partout et ailleurs</h1>}
-        </section>
-    )
-}
+    <section className={`banner ${isHome ? "homepage" : "about"}`}>
+      {isHome && (
+        <h1 className="banner__text">Chez vous, partout et ailleurs</h1>
+      )}
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
